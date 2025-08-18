4c847451828ebd7c718551a7c20fe4c95181f43f
Gabriela Jussana <gabrielajussana@gmail.com>
2025-08-18 17:55:38 -0300

    feat: POST para criar veículo

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ using MinimalApi.Dominio.Servicos;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
 
+# region Builder
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,10 +29,14 @@ builder.Services.AddDbContext<DbContexto>(
 );
 
 var app = builder.Build();
+#endregion
 
+# region Home
 app.MapGet("/", () => Results.Json(new Home{}));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+# region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
     if (administradorServico.Login(loginDTO) != null)
     {
@@ -40,9 +47,29 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
         return Results.Unauthorized();
     }
 });
+#endregion
 
+# region Veiculos
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano,
+    };
+
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculos/{veiculo.Id}", veiculo);
+});
+
+#endregion
+
+# region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
-
+#endregion
