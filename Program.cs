41bde35b2a8ec06b2de520bd52096c07d8985499
Gabriela Jussana <gabrielajussana@gmail.com>
2025-08-18 18:18:59 -0300

    feat: PUT para atualizar veículo

--- Program.cs
+++ Program.cs
@@ -74,15 +74,31 @@ app.MapGet("/veiculos", (IVeiculoServico veiculoServico, [FromQuery] int? pagina
 }).WithTags("Veículos");
 
 
-app.MapGet("/veiculos/{id}", ([FromRoute] int id,IVeiculoServico veiculoServico) =>
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
 {
     var veiculo = veiculoServico.BuscaPorId(id);
 
     if (veiculo == null) return Results.NotFound();
 
     return Results.Ok(veiculo);
+
+}).WithTags("Veículos");
+
+app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = veiculoServico.BuscaPorId(id);
+    if (veiculo == null) return Results.NotFound();
+
+    veiculo.Nome = veiculoDTO.Nome;
+    veiculo.Marca = veiculoDTO.Marca;       
+    veiculo.Ano = veiculoDTO.Ano;
+    
+    veiculoServico.Atualizar(veiculo);
+    
+    return Results.Ok(veiculo);
     
 }).WithTags("Veículos");
+
 
 #endregion
 
