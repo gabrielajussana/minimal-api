cb9186c23447a6738ce92fd24a08ea4a2a8dacb0
Gabriela Jussana <gabrielajussana@gmail.com>
2025-08-18 14:40:03 -0300

    feat: Configurando o projeto com Entity Framework e tabela de administradores

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,33 @@
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
+
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration? _configuracaoAppSettings;
+
+    public DbContexto() { } 
+
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSettings?.GetConnectionString("mysql")
+                ?? "server=localhost;database=seubanco;user=seuusuario;password=suasenha"; // ajuste conforme necessário
+
+            optionsBuilder.UseMySql(
+                stringConexao,
+                ServerVersion.AutoDetect(stringConexao)
+            );
+        }
+    }
 }
