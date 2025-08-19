fbc826c2915890654ddce8d53a3b2c0edd99f1e3
Gabriela Jussana <gabrielajussana@gmail.com>
2025-08-19 13:02:26 -0300

    test: criando teste de unidade para modelo de Administrador

---
+++ Test/Dominio/Entidades/AdministradorTest.cs
@@ -0,0 +1,27 @@
+using MinimalApi.Dominio.Entidades;
+
+namespace Tests.Dominio.Entidades;
+
+[TestClass]
+public class AdministradorTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        // Arrange
+        var adm = new Administrador();
+
+        //Act
+        adm.Id = 1;
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste123";
+        adm.Perfil = "Adm";
+
+        //Assert
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("teste123", adm.Senha);
+        Assert.AreEqual("Adm", adm.Perfil);
+
+    }
+}
